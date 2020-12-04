617cf9fea0109a0d5c44dc5300da0bda87548104
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-12-05 00:25:07 +0100

    Add files via upload

---
+++ kolekcije1/kolekcije1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace kolekcije1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            // Dodajemo elemente u kolekciju
+            al.Add(4);
+            al.Add("C# i .NET");
+            al.Insert(1, "Programiranje");
+            Console.WriteLine("--Elementi kolekcije na početku:");
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            // Uklanjamo elemente iz kolekcije
+            if (al.Contains(4))
+            {
+                al.Remove(4);
+            }
+            int pozicija = al.IndexOf("Programiranje");
+            if (pozicija >= 0)
+            {
+                al.RemoveAt(pozicija);
+            }
+            Console.WriteLine("\n-- Elementi kolekcije na završetku:");
+
+            for (int i=0;i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+        }
+    }
+}
